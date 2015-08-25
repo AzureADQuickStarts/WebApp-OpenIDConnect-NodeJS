a96e0a65ed93f92c8c766d178eb225dcc4e3c202
Brandon Werner <brandwe@microsoft.com>
2015-08-25 11:19:28 -0700

    Inital commit

---
+++ config.js
@@ -0,0 +1,10 @@
+  // Don't commit this file to your public repos. This config is for first-run
+  //
+ exports.creds = {
+ 	returnURL: 'http://localhost:3000/auth/openid/return',
+ 	identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
+ 	realm: 'http://localhost:3000',
+ 	issuer: 'https://sts.windows.net/<your app ID>',
+ 	clientID: '<Your app ID>',
+ 	clientSecret: '<Your secret>'
+ };
