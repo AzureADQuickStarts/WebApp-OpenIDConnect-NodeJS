8cde3322deab051fd008a4eb1945c58136adcbe8
Sijun Liu <sijliu@microsoft.com>
2016-10-09 17:42:35 -0700

    add parameters

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 exports.creds = {
   // Required
-  identityMetadata: 'https://login.microsoftonline.com/<tenant_name>.onmicrosoft.com/.well-known/openid-configuration', 
+  identityMetadata: 'https://login.microsoftonline.com/sijun.onmicrosoft.com/.well-known/openid-configuration', 
   // or equivalently: 'https://login.microsoftonline.com/<tenant_guid>/.well-known/openid-configuration'
   //
   // or you can use the common endpoint
@@ -9,7 +9,7 @@ exports.creds = {
   // To use the common endpoint, you have to either set `validateIssuer` to false, or provide the `issuer` value.
 
   // Required, the client ID of your app in AAD  
-  clientID: '<your_client_id>',
+  clientID: '683ead13-3193-43f0-9677-d727c25a588f',
 
   // Required, must be 'code', 'code id_token', 'id_token code' or 'id_token' 
   responseType: 'id_token', 
@@ -25,13 +25,13 @@ exports.creds = {
   
   // Required if `responseType` is 'code', 'id_token code' or 'code id_token'. 
   // If app key contains '\', replace it with '\\'.
-  clientSecret: '<your_client_secret>', 
+  clientSecret: 'X8TynX/Jo06ZepNFgLNvwCu9gYK/HRj1sJn+P96spDw=', 
 
   // Required to set to false if you don't want to validate issuer
   validateIssuer: true,
 
   // Required if you want to provide the issuer(s) you want to validate instead of using the issuer from metadata
-  issuer: null,
+  issuer: ['https://sts.windows.net/268da1a1-9db4-48b9-b1fe-683250ba90cc/'],
 
   // Required to set to true if the `verify` function has 'req' as the first parameter
   passReqToCallback: false,
