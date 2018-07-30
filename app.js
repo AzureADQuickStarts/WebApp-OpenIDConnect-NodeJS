0bd44356fffd8f5a24fd9b81a83ceef953884224
Takashi Takebayashi <changesworlds@gmail.com>
2018-07-30 09:44:14 +0900

    Update complete to use passport-azure-ad 3.0.x

--- app.js
+++ app.js
@@ -47,53 +47,41 @@ var log = bunyan.createLogger({
 //   serialize users into and deserialize users out of the session.  Typically,
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.email);
-});
 
-passport.deserializeUser(function(id, done) {
-  findByEmail(id, function (err, user) {
-    done(err, user);
-  });
-});
 
-// array to hold logged in users
-var users = [];
+// Use the OIDCStrategy within Passport. (Section 2)
 
-var findByEmail = function(email, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-   log.info('we are using user: ', user);
-    if (user.email === email) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-};
-
-// Use the OIDCStrategy within Passport. (Section 2) 
-// 
-//   Strategies in passport require a `validate` function, which accept
-//   credentials (in this case, an OpenID identifier), and invoke a callback
+//   Strategies in passport require a `validate` function that accepts
+//   credentials (in this case, an OpenID identifier), and invokes a callback
 //   with a user object.
 passport.use(new OIDCStrategy({
-    callbackURL: config.creds.returnURL,
-    realm: config.creds.realm,
-    clientID: config.creds.clientID,
-    clientSecret: config.creds.clientSecret,
-    oidcIssuer: config.creds.issuer,
     identityMetadata: config.creds.identityMetadata,
-    skipUserProfile: config.creds.skipUserProfile,
+    clientID: config.creds.clientID,
     responseType: config.creds.responseType,
-    responseMode: config.creds.responseMode
+    responseMode: config.creds.responseMode,
+    redirectUrl: config.creds.redirectUrl,
+    allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
+    clientSecret: config.creds.clientSecret,
+    validateIssuer: config.creds.validateIssuer,
+    isB2C: config.creds.isB2C,
+    issuer: config.creds.issuer,
+    passReqToCallback: config.creds.passReqToCallback,
+    scope: config.creds.scope,
+    loggingLevel: config.creds.loggingLevel,
+    loggingNoPII: config.creds.loggingNoPII,
+    nonceLifetime: config.creds.nonceLifetime,
+    nonceMaxAmount: config.creds.nonceMaxAmount,
+    useCookieInsteadOfSession: config.creds.useCookieInsteadOfSession,
+    cookieEncryptionKeys: config.creds.cookieEncryptionKeys,
+    clockSkew: config.creds.clockSkew,
   },
   function(iss, sub, profile, accessToken, refreshToken, done) {
-    if (!profile.email) {
-      return done(new Error("No email found"), null);
+    if (!profile.oid) {
+      return done(new Error("No oid found"), null);
     }
     // asynchronous verification, for effect...
     process.nextTick(function () {
-      findByEmail(profile.email, function(err, user) {
+      findByOid(profile.oid, function(err, user) {
         if (err) {
           return done(err);
         }
@@ -108,12 +96,39 @@ passport.use(new OIDCStrategy({
   }
 ));
 
-
 // configure Express (Section 2)
+// Passport session setup. (Section 2)
+//   To support persistent sign-in sessions, Passport needs to be able to
+//   serialize users into the session and deserialize them out of the session. Typically,
+//   this is done simply by storing the user ID when serializing and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+    done(null, user.oid);
+});
+
+passport.deserializeUser(function(id, done) {
+    findByOid(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+// array to hold signed-in users
+var users = [];
+
+var findByEmail = function(email, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        log.info('we are using user: ', user);
+        if (user.oid === oid) {
+            return fn(null, user);
+        }
+    }
+    return fn(null, null);
+};
+
+// configure Express (section 2)
 
 var app = express();
-
-
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
@@ -130,7 +145,7 @@ app.configure(function() {
   app.use(express.static(__dirname + '/../../public'));
 });
 
-//Routes (Section 4)
+//Routes (section 4)
 
 app.get('/', function(req, res){
   res.render('index', { user: req.user });
@@ -140,25 +155,12 @@ app.get('/account', ensureAuthenticated, function(req, res){
   res.render('account', { user: req.user });
 });
 
-app.get('/login',
-  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
+app.get('/login', 
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/' }),
   function(req, res) {
     log.info('Login was called in the Sample');
     res.redirect('/');
 });
-
-// GET /auth/openid/return
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-app.get('/auth/openid/return',
-  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
-  function(req, res) {
-    log.info('We received a return from AzureAD.');
-    res.redirect('/');
-  });
-
 
 // POST /auth/openid/return
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -166,30 +168,38 @@ app.get('/auth/openid/return',
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.post('/auth/openid/return',
-  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/' }),
   function(req, res) {
-    log.info('We received a return from AzureAD.');
+    log.info('call post auth return');
     res.redirect('/');
-  });
+  }
+);
 
-app.get('/logout', function(req, res){
-req.session.destroy(function (err) {
-    req.logOut();
-    res.redirect('https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=http://localhost:3000');
-  });
-
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
-app.listen(3000);
+// Simple route middleware to ensure user is authenticated. (section 4)
 
-
-// Simple route middleware to ensure user is authenticated. (Section 4)
-
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
+//   Use this route middleware on any resource that needs to be protected. If
+//   the request is authenticated (typically via a persistent sign-in session),
+//   the request proceeds. Otherwise, the user is redirected to the
+//   sign-in page.
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
 }
+
+var findByOid = function(oid, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    log.info('we are using user: ', user);
+    if (user.oid === oid) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+};
+
+app.listen(3000);
